da1906a3e6641274f21bf284ff2bf06e6c57e1f8
jose9pmb <91058872+jose9pmb@users.noreply.github.com>
2021-09-24 23:06:23 +0200

    ClasesEscaner

---
+++ src/ClasesEscaner.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class ClasesEscaner {
+    public static void main(String[] args) {
+      /*  System.out.println("Escribe tu nombre: ");
+        Scanner lectura = new Scanner(System.in);
+        var nombre = lectura.nextLine();*/
+      byte varByte = 5 ;
+        System.out.println("Rango del byte: " + Byte.MIN_VALUE);
+        System.out.println("Rango del byte: " + Byte.MAX_VALUE);
+    }
+    
+}
